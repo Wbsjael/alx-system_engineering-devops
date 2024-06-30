0f3c5b3be8ec6a5cf6d95cfa08c443c600298c4f
Wbsjael <muchemijessica77@gmail.com>
2024-06-30 14:58:52 -0700

    configuration

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,20 +1,6 @@
 #!/usr/bin/pup
-# a manifest that installs package flask
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-package { 'flask':
+# We use Puppet to install flask from pip3.
+package { 'flask' :
   ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip',
-  require  => Package['python3-pip'],
-}
-
-package { 'werkzeug':
-  ensure    => '2.1.1',
-  provider  => 'pip',
-  name      => 'werkzeug',
-  require   => Package['python3-pip'],
+  provider => 'pip3',
 }
